a63966eb66f7c7f26610a4317a620be4cdecdc49
sumit tiwari <sumit.kumar@hiveminds.in>
2025-11-08 20:27:28 +0530

    this is my first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(4);
+        System.out.println(6);
+        System.out.println(6/0);
+    }
+}
